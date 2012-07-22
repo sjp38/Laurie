c65a0c92c7a772071ec697f6fcd58d4668dae7f9
SeongJae Park <sj38.park@gmail.com>
2012-07-22 14:57:24 +0900

    Add HOME/RECENT APPS enable/disable feature.

--- src/org/drykiss/android/app/laurie/LaurieActivity.java
+++ src/org/drykiss/android/app/laurie/LaurieActivity.java
@@ -3,19 +3,59 @@ package org.drykiss.android.app.laurie;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.LinearLayout;
 
 import org.drykiss.android.app.laurie.ad.AdvertisementManager;
 
 public class LaurieActivity extends Activity {
+    private SharedPreferences.Editor mPrefsEditor;
+
+    private CompoundButton.OnCheckedChangeListener mFeatureCheckCangeListener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            switch (buttonView.getId()) {
+                case R.id.homeFeatureCheckBox:
+                    mPrefsEditor.putBoolean(LaurieService.KEY_TURN_ON_HOME_BUTTON, isChecked);
+                    mPrefsEditor.commit();
+                    break;
+                case R.id.recentAppsFeaturecheckBox:
+                    mPrefsEditor
+                            .putBoolean(LaurieService.KEY_TURN_ON_RECENT_APPS_BUTTON, isChecked);
+                    mPrefsEditor.commit();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
     View mAdView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        mPrefsEditor = PreferenceManager.getDefaultSharedPreferences(
+                getApplicationContext()).edit();
+
+        CheckBox featureCheckBox = (CheckBox) findViewById(R.id.homeFeatureCheckBox);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(
+                getApplicationContext());
+        featureCheckBox.setChecked(prefs.getBoolean(LaurieService.KEY_TURN_ON_HOME_BUTTON, true));
+        featureCheckBox.setOnCheckedChangeListener(mFeatureCheckCangeListener);
+
+        featureCheckBox = (CheckBox) findViewById(R.id.recentAppsFeaturecheckBox);
+        featureCheckBox.setChecked(prefs.getBoolean(LaurieService.KEY_TURN_ON_RECENT_APPS_BUTTON,
+                true));
+        featureCheckBox.setOnCheckedChangeListener(mFeatureCheckCangeListener);
 
         startService(new Intent(getApplicationContext(), LaurieService.class));
 
--- src/org/drykiss/android/app/laurie/LaurieService.java
+++ src/org/drykiss/android/app/laurie/LaurieService.java
@@ -7,11 +7,36 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 
 public class LaurieService extends Service {
     static final int NOTI_HOME_ID = 1;
     static final int NOTI_RECENT_ID = 2;
+
+    static final String KEY_TURN_ON_HOME_BUTTON = "org.drykiss.android.app.laurie.TURN_ON_HOME_BUTTON";
+    static final String KEY_TURN_ON_RECENT_APPS_BUTTON = "org.drykiss.android.app.laurie.TURN_ON_RECENT_APPS_BUTTON";
+
+    private NotificationManager mNotiManager;
+    private Notification mHomeNoti;
+    private Notification mRecentAppsNoti;
+
+    private SharedPreferences mPrefs;
+
+    private SharedPreferences.OnSharedPreferenceChangeListener mPrefsChangedListener =
+            new SharedPreferences.OnSharedPreferenceChangeListener() {
+                @Override
+                public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                        String key) {
+                    if (KEY_TURN_ON_HOME_BUTTON.equals(key)
+                            || KEY_TURN_ON_RECENT_APPS_BUTTON.equals(key)) {
+                        turnOnOffFeatures(
+                                sharedPreferences.getBoolean(KEY_TURN_ON_HOME_BUTTON, true),
+                                sharedPreferences.getBoolean(KEY_TURN_ON_RECENT_APPS_BUTTON, true));
+                    }
+                }
+            };
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -22,27 +47,40 @@ public class LaurieService extends Service {
     public void onCreate() {
         super.onCreate();
 
+        mPrefs = PreferenceManager
+                .getDefaultSharedPreferences(getApplicationContext());
+
         Intent homeIntent = new Intent(Intent.ACTION_MAIN, null);
         homeIntent.addCategory(Intent.CATEGORY_HOME);
         homeIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
                 | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
         PendingIntent homeContentIntent = PendingIntent.getActivity(this, 0, homeIntent, 0);
 
-        Notification homeNoti = buildLaurieNoti(R.drawable.ic_noti_home, getText(R.string.ticker),
+        mHomeNoti = buildLaurieNoti(R.drawable.ic_noti_home, getText(R.string.ticker),
                 System.currentTimeMillis(), getText(R.string.home_content_title),
                 getText(R.string.home_content), homeContentIntent);
 
         Intent recentIntent = new Intent(this, RecentAppsActivity.class);
+        recentIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         PendingIntent recentContentIntent = PendingIntent.getActivity(this, 0, recentIntent, 0);
 
-        Notification recentNoti = buildLaurieNoti(R.drawable.ic_noti_recent,
+        mRecentAppsNoti = buildLaurieNoti(R.drawable.ic_noti_recent,
                 getText(R.string.ticker),
                 System.currentTimeMillis(), getText(R.string.recent_apps_content_title),
                 getText(R.string.recent_apps_content), recentContentIntent);
 
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(NOTI_HOME_ID, homeNoti);
-        notificationManager.notify(NOTI_RECENT_ID, recentNoti);
+        mNotiManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mPrefs.registerOnSharedPreferenceChangeListener(mPrefsChangedListener);
+        turnOnOffFeatures(mPrefs.getBoolean(KEY_TURN_ON_HOME_BUTTON, true),
+                mPrefs.getBoolean(KEY_TURN_ON_RECENT_APPS_BUTTON, true));
+    }
+
+    @Override
+    public void onDestroy() {
+        mPrefs.unregisterOnSharedPreferenceChangeListener(mPrefsChangedListener);
+        mNotiManager.cancelAll();
+        super.onDestroy();
     }
 
     private Notification buildLaurieNoti(int icon, CharSequence tickerText, long when,
@@ -53,5 +91,19 @@ public class LaurieService extends Service {
         noti.flags |= Notification.FLAG_NO_CLEAR;
 
         return noti;
+    }
+
+    private void turnOnOffFeatures(boolean turnOnHome, boolean turnOnRecentApps) {
+        if (turnOnHome) {
+            mNotiManager.notify(NOTI_HOME_ID, mHomeNoti);
+        } else {
+            mNotiManager.cancel(NOTI_HOME_ID);
+        }
+
+        if (turnOnRecentApps) {
+            mNotiManager.notify(NOTI_RECENT_ID, mRecentAppsNoti);
+        } else {
+            mNotiManager.cancel(NOTI_RECENT_ID);
+        }
     }
 }
